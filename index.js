a4199ec0684e2b6054e3b930bb986948dbe71d52
Max Ogden <max@maxogden.com>
2014-04-08 11:53:35 -0700

    update to use newer dat

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // <vehicle id="1098" routeTag="51B" lat="37.868206" lon="-122.2973022" secsSinceReport="34" predictable="true" heading="255" speedKmHr="2"/>
 var request = require('request')
 var parseString = require('xml2js').parseString
-var crypto = require('crypto')
 
 module.exports = function (agency, interval, onNewLocation) {
   var baseURL = "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=" + agency
@@ -9,14 +8,20 @@ module.exports = function (agency, interval, onNewLocation) {
   makeRequest()
   
   function makeRequest() {
-    var now = Date.now() - interval
-    request(baseURL + "&t=" + now, function(err, resp, xml) {
+    request(baseURL + "&t=0", function(err, resp, xml) {
       if (err) return onNewLocation(err)
-      console.log('debug', xml)
-      xml.split('\n').map(function(line) {
+      var lastTime = +new Date()
+      xml.split('\n').reverse().map(function(line) {
         if (line.indexOf('<vehicle') > -1) {
           parseString(line, function (err, result) {
+            if (err) return onNewLocation(err)
             var bus = result.vehicle.$
+            var secs = +bus.secsSinceReport
+            var adjusted = lastTime - (secs * 1000)
+            adjusted = adjusted.toString().split('')
+            adjusted = adjusted.slice(0, adjusted.length - 3)
+            adjusted = +adjusted.join('') + '000'
+            bus.lastUpdated = adjusted
             onNewLocation(null, bus)
           })
         }
--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 var createFeed = require('./')
+var Dat = require('dat')
+var crypto = require('crypto')
 
-createFeed('actransit', 1000 * 45, function onLocation(err, location) {
+var dat = new Dat('./data/', function(err) {
   if (err) throw err
-  console.log(JSON.stringify(location))
+  createFeed('actransit', 1000 * 5, function onLocation(err, location) {
+    if (err) return console.error('error', err)
+    
+    var ws = dat.createWriteStream({ objects: true, primary: ['lastUpdated', 'id', 'lat', 'lon'], separator: '|' })
+    
+    ws.on('error', function(e) {
+      // console.log('write err', e)
+    })
+    
+    ws.write(location)
+    ws.end()
+  })
 })
