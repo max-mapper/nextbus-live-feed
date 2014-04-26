e8afcd923d979b18066bd5200617732246bbebae
Max Ogden <max@maxogden.com>
2014-04-25 20:11:25 -0700

    use leveldown-hyper

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var createFeed = require('./')
 var Dat = require('dat')
+var hyper = require('leveldown-hyper')
 var crypto = require('crypto')
 
-var dat = new Dat('./data/', { port: process.env['PORT'] }, function(err) {
+var dat = new Dat('./data/', { port: process.env['PORT'], backend: hyper }, function(err) {
   if (err) throw err
   createFeed('actransit', 1000 * 5, function onLocation(err, location) {
     if (err) return console.error('error', err)
